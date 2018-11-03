0ab20f24b44011dd62a96cf726a1f59212ffcb1e
EddieLo <eddielo106@gmail.com>
2018-11-02 18:11:28 -0700

    outline for login modal

--- client/components/LoginRegister.jsx
+++ client/components/LoginRegister.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from '../styles/LoginRegister';
 
 const LoginRegister = () => (
-  <div>
-    Hello
+  <div className={styles.container}>
+    {'Don\'t have an account?'}
+    <em> Register Now!</em>
+    <br />
+    <button
+      type="button"
+      className={styles.button}
+    >
+      Register
+    </button>
   </div>
 );
 
