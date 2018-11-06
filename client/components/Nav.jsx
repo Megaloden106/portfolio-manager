991812242ca059760ba3afebd9ca86ba25cc11af
EddieLo <eddielo106@gmail.com>
2018-11-05 20:38:14 -0800

    add nav box

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,24 +4,26 @@ import handleLoginModal from '../actions/handleModal';
 import styles from '../styles/Nav';
 
 const Nav = ({ user, loginModal, handleLoginClick }) => (
-  <nav className={styles.navContainer}>
-    <ul className={styles.list}>
-      <li className={styles.item}>Home</li>
-    </ul>
-    {!user && (
-      <div className={styles.account}>
-        <button
-          type="button"
-          onClick={() => handleLoginClick(loginModal)}
-          id="loginSubmit"
-        >
-          <b>Login</b>
-        </button>
-        or
-        <button type="button"><b>Signup</b></button>
-      </div>
-    )}
-  </nav>
+  <div className={styles.navContainer}>
+    <nav className={styles.navBar}>
+      <ul className={styles.list}>
+        <li className={styles.item}>Home</li>
+      </ul>
+      {!user && (
+        <div className={styles.account}>
+          <button
+            type="button"
+            onClick={() => handleLoginClick(loginModal)}
+            id="loginSubmit"
+          >
+            <b>Login</b>
+          </button>
+          or
+          <button type="button"><b>Signup</b></button>
+        </div>
+      )}
+    </nav>
+  </div>
 );
 
 const mapStateToProps = state => ({
