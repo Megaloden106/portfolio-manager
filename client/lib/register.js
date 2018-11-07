539f776b46def8b7c5fc8f25dee9ba69542259d6
EddieLo <eddielo106@gmail.com>
2018-11-06 22:18:29 -0800

    finish register new user

--- client/lib/register.js
+++ client/lib/register.js
@@ -6,13 +6,19 @@ import {
   sha512,
 } from './hash';
 
+const formatString = (string) => {
+  let result = string.toLowerCase();
+  result = result[0].toUpperCase() + string.slice(1);
+  return result;
+};
+
 const register = (creds) => {
   const salt = genRandomString(25);
   const password = sha512(pepperS + creds.password, salt + pepperE);
   return axios.post('/api/user', {
-    name: `${creds.firstName} ${creds.lastName}`,
+    name: `${formatString(creds.firstName)} ${formatString(creds.lastName)}`,
     email: creds.email,
-    username: creds.username,
+    username: formatString(creds.username),
     password,
     salt,
   }).catch(({ response }) => response.data);
